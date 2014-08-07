67ae61c47608e47d10394eae5f60961f872d7768
sm-valery <sm-valery@yandex.ru>
2014-08-07 15:44:28 +0400

    07082014

---
+++ WebApplication1/Controllers/SettingsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using gTravel.Models;
+
+namespace gTravel.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private goDbEntities db = new goDbEntities();
+
+        // GET: Settings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region Currency
+        public ActionResult Currency()
+        {
+
+            return View(db.Currencies.ToList());
+        }
+
+        public ActionResult Currency_create()
+        {
+            Currency c = new Currency();
+
+            return View(c);
+        }
+
+        [HttpPost]
+        public ActionResult Currency_create(Currency c)
+        {
+            if(ModelState.IsValid)
+            {
+                c.CurrencyId = Guid.NewGuid();
+
+                db.Currencies.Add(c);
+                db.SaveChanges();
+
+                return RedirectToAction("Currency");
+            }
+
+            return View(c);
+        }
+        
+        #endregion
+        
+        #region seria
+        public ActionResult seria()
+        {
+            return View(db.serias.ToList());
+        }
+
+        public ActionResult Seria_Create()
+        {
+            seria s = new seria();
+           
+
+            return View(s);
+        }
+
+        [HttpPost]
+        public ActionResult Seria_Create(seria s)
+        {
+
+            if(ModelState.IsValid)
+            {
+                s.SeriaId = Guid.NewGuid();
+
+                db.serias.Add(s);
+                db.SaveChanges();
+
+                return RedirectToAction("seria");
+            }
+
+            return View(s);
+        }
+
+        #endregion
+
+        public ActionResult Territory()
+        {
+            return View(db.Territories.ToList());
+        }
+
+        public ActionResult Territory_Create()
+        {
+            Territory t = new Territory();
+
+            return View(t);
+        }
+
+        [HttpPost]
+        public ActionResult Territory_Create(Territory t)
+        {
+            if(ModelState.IsValid)
+            {
+                t.TerritoryId = Guid.NewGuid();
+
+                db.Territories.Add(t);
+                db.SaveChanges();
+
+                return RedirectToAction("territory");
+            }
+
+            return View(t);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/testController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using gTravel.Models;
+
+namespace gTravel.Controllers
+{
+    public class testController : Controller
+    {
+        private goDbEntities db = new goDbEntities();
+
+        // GET: test
+        public ActionResult Index()
+        {
+            return View(db.serias.ToList());
+        }
+
+        // GET: test/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            seria seria = db.serias.Find(id);
+            if (seria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seria);
+        }
+
+        // GET: test/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: test/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SeriaId,Code,Name")] seria seria)
+        {
+            if (ModelState.IsValid)
+            {
+                seria.SeriaId = Guid.NewGuid();
+                db.serias.Add(seria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(seria);
+        }
+
+        // GET: test/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            seria seria = db.serias.Find(id);
+            if (seria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seria);
+        }
+
+        // POST: test/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SeriaId,Code,Name")] seria seria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seria);
+        }
+
+        // GET: test/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            seria seria = db.serias.Find(id);
+            if (seria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seria);
+        }
+
+        // POST: test/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            seria seria = db.serias.Find(id);
+            db.serias.Remove(seria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
