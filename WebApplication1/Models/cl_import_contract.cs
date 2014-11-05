4a0346daa4b23998bbb97186accd9c25e37da59e
sm-valery <sm-valery@yandex.ru>
2014-11-05 22:40:05 +0300

    2

---
+++ WebApplication1/Models/cl_import_contract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gTravel.Models
+{
+    public class cl_import_contract
+    {
+        public int contract_number { get; set; }
+        public string contract_number_str { get; set; }
+        public DateTime? date_out { get; set; }
+        public DateTime? date_begin { get; set; }
+        public DateTime? date_end { get; set; }
+        public string SubjName { get; set; }
+        public DateTime? dateofbirth { get; set; }
+        public string pasport { get; set; }
+        public string gender { get; set; }
+        public string placeofbirth { get; set; }
+        public DateTime? passportvaliddate { get; set; }
+    }
+}
