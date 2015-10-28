a30fd8448d68bef4ce8dda5e3b023014d5b13885
sm-valery <sm-valery@yandex.ru>
2015-10-28 17:29:35 +0300

    2810

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -10,3 +10,7 @@ join ContractAgent ca on ca.ContractId = c.ContractId
 join Agent ag on ag.AgentId = ca.AgentId
 join ContractRisk cr on cr.ContractId = c.ContractId
 where st.Code in ('confirmed','annul')
+
+
+update
+
---
+++ SQLQuery_update_agent_prem.sql
@@ -0,0 +1,19 @@
+﻿update ca 
+set ca.insprem = r.insprem
+from ContractAgent ca
+join 
+(
+select sum(cr.insprem) insprem, cr.ContractId from contractrisk cr group by cr.ContractId
+) r on r.ContractId = ca.ContractId
+where ca.insprem = 0 or ca.insprem is null
+
+
+
+update ca
+set ca.inspremrur = round( ca.InsPrem * (select top 1 rate from CurRate where CurRate.RateDate = cast( c.date_out as date)),2)
+from ContractAgent ca
+join Contract c on c.ContractId = ca.ContractId
+where ca.InsPremRur is null
+
+
+select * from ContractAgent where insprem is not null
